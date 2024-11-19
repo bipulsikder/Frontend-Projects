c57336b631ed99398e61e8231d3431bae976542d
Bipul <147647143+bipulsikder@users.noreply.github.com>
2024-11-20 00:02:42 +0530

    Update script.js

--- GOLF_Pra/script.js
+++ GOLF_Pra/script.js
@@ -1,10 +1,10 @@
 var crsr = document.querySelector("#cursor")
 var crsrBlur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
-  crsr.style.left = dets.x +"px"
-  crsr.style.top = dets.y +"px"
-  crsrBlur.style.left = dets.x -150 +"px"
-  crsrBlur.style.top = dets.y -150 +"px"
+  crsr.style.left = dets.x -10+"px"
+  crsr.style.top = dets.y -10+"px"
+  crsrBlur.style.left = dets.x -250 +"px"
+  crsrBlur.style.top = dets.y -250+"px"
 })
 
 
